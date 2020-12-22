5b10f26045c76c32783bc9dbbb4142ee7bf502c0
libingyi <924216885@qq.com>
2020-12-22 13:35:04 +0800

    add Test folder

---
+++ yami-shop-admin/src/main/java/com/yami/shop/admin/test/Java8Test.java
@@ -0,0 +1,13 @@
+package com.yami.shop.admin.test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Java8Test {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+        List<String> filtered = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.toList());
+        System.out.println(filtered);
+    }
+}
